73e398ad7a7f07feb01d23829ff7dbf409a24b46
Kato Megumi <1465439890@qq.com>
2025-04-22 03:38:38 +0800

    写了一些请求

--- src/views/DashboardChart.tsx
+++ src/views/DashboardChart.tsx
@@ -1,5 +1,5 @@
 import FunnelAnalysis from '../components/FunnelAnalysis';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from '../styles/DashboardChart.module.css';
 import { Card } from 'antd';
 import RFMCustomerAnalysis from '../components/RFMCustomerAnalysis';
@@ -8,8 +8,39 @@ import CategoryCnt from '../components/CategoryCnt';
 import ItemCnt from '../components/ItemCnt';
 import DailyTrafficAnalysis from '../components/DailyTrafficAnalysis';
 import HourlyTrafficAnalysis from '../components/HourlyTrafficAnalysis';
+import instance from '../request/request';
 
 function DashboardChart() {
+    const [userData, setUserData] = useState(null)
+    const [buyAll, setBuyAll] = useState(null)
+    const [loadingUser, setLoadingUser] = useState(true)
+    const [loadingBuyAll, setLoadingBuyAll] = useState(true)
+
+    useEffect(() => {
+        instance.get('http://localhost:8080/day/allUser').then(res => {
+            if (res.status === 200) {
+                setUserData(res.data)
+            } else {
+                console.log("请求出错！");
+            }
+        }).finally(() => {
+            setLoadingUser(false)
+        })
+    }, [])
+
+    useEffect(() => {
+        instance.get('http://localhost:8080/top10category/buyAll').then(res => {
+            if (res.status === 200) {
+                setBuyAll(res.data)
+            } else {
+                console.log("请求出错！");
+            }
+        }).finally(() => {
+            setLoadingBuyAll(false)
+        })
+    }, [])
+
+
     return (
         <div className={style.body}>
             <div className={style.title}>
@@ -39,19 +70,19 @@ function DashboardChart() {
                     <div className={style.main2_1}>
                         <div className={style.main2_1_1}>
                             <Card variant='borderless' style={{ width: '100%', height: '100%' }}>
-                                <h2>1000000000</h2>
+                                <h2 style={{ color: 'blue' }}>100000000</h2>
                                 <p>数据总量</p>
                             </Card>
                         </div>
                         <div className={style.main2_1_2}>
                             <Card variant='borderless' style={{ width: '100%', height: '100%' }}>
-                                <h2>1000000000</h2>
+                                {loadingUser ? (<h4 style={{ color: 'red' }}>正在分析数据...</h4>) : (<h2 style={{ color: 'blue' }}>{userData}</h2>)}
                                 <p>用户总量</p>
                             </Card>
                         </div>
                         <div className={style.main2_1_3}>
                             <Card variant='borderless' style={{ width: '100%', height: '100%' }}>
-                                <h2>1000000000</h2>
+                                {loadingBuyAll ? (<h4 style={{ color: 'red' }}>正在分析数据...</h4>) : (<h2 style={{ color: 'blue' }}>{buyAll}</h2>)}
                                 <p>购买数量</p>
                             </Card>
                         </div>
